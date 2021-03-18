568cfb6fa93a548924938301ecf1a7ddad672ef4
Nathan McKenzie <nathantaylormckenzie@hotmail.com>
2021-03-17 17:48:25 -0700

    Adding SignUp Code

--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -2,8 +2,11 @@ import React, { useRef, useState } from "react";
 import { Form, Button, Card, Alert } from "react-bootstrap";
 import { useAuth } from "../contexts/AuthContext";
 import { Link, useHistory } from "react-router-dom";
+import { app, firebaseAuth } from "../base";
 
 export default function Signup() {
+  const firstNameRef = useRef();
+  const lastNameRef = useRef();
   const emailRef = useRef();
   const passwordRef = useRef();
   const passwordConfirmRef = useRef();
@@ -15,6 +18,9 @@ export default function Signup() {
   async function handleSubmit(e) {
     e.preventDefault();
 
+    const db = app.firestore();
+    const current = firebaseAuth.currentUser.email;
+
     if (passwordRef.current.value !== passwordConfirmRef.current.value) {
       return setError("Passwords do not match");
     }
@@ -23,8 +29,26 @@ export default function Signup() {
       setError("");
       setLoading(true);
       await signup(emailRef.current.value, passwordRef.current.value);
+      await db
+        .collection("users")
+        .doc(emailRef.current.value)
+        .set(
+          {
+            firstName: firstNameRef.current.value,
+            lastName: lastNameRef.current.value,
+          },
+          { merge: true }
+        )
+        .then(() => {
+          console.log("Document successfully written!");
+        })
+        .catch((error) => {
+          console.error("Error writing document: ", error);
+        });
       history.push("/personalityquiz");
-    } catch {
+    } catch (error) {
+      console.log("ERROR", error);
+
       setError("Failed to create an account");
     }
 
@@ -38,6 +62,14 @@ export default function Signup() {
           <h2 className="text-center mb-4">Sign Up</h2>
           {error && <h3 className="signup-error">{error}</h3>}
           <Form onSubmit={handleSubmit}>
+            <Form.Group id="first-name">
+              <Form.Label>First Name</Form.Label>
+              <Form.Control type="text" ref={firstNameRef} required />
+            </Form.Group>
+            <Form.Group id="last-name">
+              <Form.Label>Last Name</Form.Label>
+              <Form.Control type="text" ref={lastNameRef} required />
+            </Form.Group>
             <Form.Group id="email">
               <Form.Label>Email</Form.Label>
               <Form.Control type="email" ref={emailRef} required />
